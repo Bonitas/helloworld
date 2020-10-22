293ebd37c65dca4a4cee9400195fe3c62fad332b
Waldemar Kindler <waldemar.kindler@lidl.com>
2020-10-22 15:42:38 +0200

    Test

--- main.go
+++ main.go
@@ -1,11 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
-func helloworld() string {
-	return "Hello World!!"
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
+
+func listenUntilShutdown(s *http.Server, shutdownTimeout time.Duration) {
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGTERM)
+
+		<-sig
+
+		log.Printf("[entrypoint] SIGTERM received.. shutting down server in %s\n", shutdownTimeout.String())
+
+		<-time.Tick(shutdownTimeout)
+
+		if err := s.Shutdown(context.Background()); err != nil {
+			log.Printf("[entrypoint] Error in Shutdown: %v", err)
+		}
+
+		log.Printf("[entrypoint] No new connections allowed. Exiting in: %s\n", shutdownTimeout.String())
+
+		<-time.Tick(shutdownTimeout)
+
+		close(idleConnsClosed)
+	}()
+
+	// Run the HTTP server in a separate go-routine.
+	go func() {
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			log.Printf("[entrypoint] Error ListenAndServe: %v", err)
+			close(idleConnsClosed)
+		}
+	}()
+
+	<-idleConnsClosed
 }
 
 func main() {
-	fmt.Println(helloworld())
+	s := &http.Server{
+		Addr:           fmt.Sprintf(":%d", 8080),
+		ReadTimeout:    1000,
+		WriteTimeout:   1000,
+		MaxHeaderBytes: 1 << 20, // Max header of 1MB
+	}
+
+	http.HandleFunc("/", helloworld)
+
+	listenUntilShutdown(s, 1*time.Second)
 }
